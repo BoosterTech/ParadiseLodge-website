874adcd8cc9ec929d30e3e3479906b60147cbb53
Darek Podczasik <boostertech@mail.com>
2024-11-26 20:01:14 +0000

    feat: add default title & description to metadata, add google font Jasefin_sans, move Logo & Navigation components to seperate file

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,35 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 export const metadata = {
-  title: "The Paradise Lodge",
+  // title: "The Paradise Lodge",
+  title: {
+    template: "%s - The Paradise Lodge",
+    default: "Welcome / The Paradise Lodge",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests ",
 };
+
+console.log(josefin);
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body className="bg-primary-950 text-primary-100 min-h-screen ">
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright by The Paradise Lodge</footer>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col `}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
