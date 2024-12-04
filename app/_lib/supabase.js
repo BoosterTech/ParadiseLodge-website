ce7f4af4d2f5fba2259cea19cf58d146b10914d3
Darek Podczasik <boostertech@mail.com>
2024-12-04 20:15:01 +0000

    refactor: comments removed

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -1,5 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
+// const supabaseUrl = "https://snxksbzdaykibwprwxow.supabase.co";
+// const supabaseKey =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNueGtzYnpkYXlraWJ3cHJ3eG93Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNzcyMTg2NywiZXhwIjoyMDQzMjk3ODY3fQ.XWGlbL7cR7Zo-U6jTDWKnDgIncOMzqi1-pTb9N-2vew";
 export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
