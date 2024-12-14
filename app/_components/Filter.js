0f329f7aba12449fb0f4a09c493c18c5b78dd70f
Darek Podczasik <boostertech@mail.com>
2024-12-14 14:34:39 +0000

    perf: optimize active filter state with useMemo - Replaced direct assignment of ctiveFilter with useMemo to memoize the filter value. - This change improves performance by preventing unnecessary recalculations of the active filter on every render.

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -1,14 +1,14 @@
 "use client";
 
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
-import { Children } from "react";
+import { useMemo } from "react";
 
 function Filter() {
   const searchParams = useSearchParams();
   const router = useRouter();
   const pathname = usePathname();
 
-  const activeFilter = searchParams.get("capacity") ?? "all";
+  const activeFilter = useMemo(() => searchParams.get("capacity") ?? "all");
 
   function handleFilter(filter) {
     const params = new URLSearchParams(searchParams);
