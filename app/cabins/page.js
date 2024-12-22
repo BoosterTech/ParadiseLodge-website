db42d8c032e20f3bcf7b749ab597a421b77fecf2
Darek Podczasik <boostertech@mail.com>
2024-12-22 21:26:53 +0000

    feat: integrate ReservationReminder into cabins

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import CabinList from "@/app/_components/CabinList";
 import Spinner from "@/app/_components/Spinner";
 import Filter from "@/app/_components/Filter";
+import ReservationReminder from "../_components/ReservationReminder";
 
 // export const revalidate = 0; Only for statically rendered pages.Use of SearchParams makes page dynamically rendered
 
@@ -31,6 +32,7 @@ export default async function Page({ searchParams }) {
       </div>
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
