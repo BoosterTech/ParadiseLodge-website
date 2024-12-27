3c9caf831f26c3d60aca16a96d3693c8f3d4931c
Darek Podczasik <boostertech@mail.com>
2024-12-27 23:21:52 +0000

    feat: add updateProfile action for handling profile updates - Introduced updateProfile function to handle user profile updates. - Integrated uth to ensure session validation. - Added validation for ationalID format. - Enhanced functionality to parse and update nationality and country flag. - Placeholder integration for supabase and updateGuest data services.

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,7 +1,8 @@
 "use server";
 
-import { redirect } from "next/dist/server/api-utils";
-import { signIn, signOut } from "./auth";
+import { auth, signIn, signOut } from "./auth";
+import { updateGuest } from "./data-service";
+import { supabase } from "./supabase";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
@@ -9,4 +10,29 @@ export async function signInAction() {
 
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
+}
+
+export async function updateProfile(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("Please provide a valid nationalID");
+
+  const updateData = { nationality, countryFlag, nationalID };
+
+  updateGuest(session.user.guestId, updateData);
+
+  // const { data, error } = await supabase
+  //   .from("guests")
+  //   .update(updateData)
+  //   .eq("id", session.user.guestId);
+
+  // if (error) {
+  //   console.error(error);
+  //   throw new Error("Guest could not be updated");
+  // }
 }
