79ad0d22c8165b66a25756b8ef3a57e7f7e4dfaf
Darek Podczasik <boostertech@mail.com>
2024-12-28 16:41:08 +0000

    refactor: update updateProfile function to revalidate cache on profile route

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,8 +1,8 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { updateGuest } from "./data-service";
-import { supabase } from "./supabase";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
@@ -14,6 +14,7 @@ export async function signOutAction() {
 
 export async function updateProfile(formData) {
   const session = await auth();
+
   if (!session) throw new Error("You must be logged in");
 
   const nationalID = formData.get("nationalID");
@@ -26,13 +27,5 @@ export async function updateProfile(formData) {
 
   updateGuest(session.user.guestId, updateData);
 
-  // const { data, error } = await supabase
-  //   .from("guests")
-  //   .update(updateData)
-  //   .eq("id", session.user.guestId);
-
-  // if (error) {
-  //   console.error(error);
-  //   throw new Error("Guest could not be updated");
-  // }
+  revalidatePath("/account/profile");
 }
